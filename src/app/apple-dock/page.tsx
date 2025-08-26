81706fed8e304ee858ac23cb92f77d7f442cb42e
Jamie Degnan <jamie@mondoo.com>
2025-08-26 16:33:48 -0700

    WIP

--- src/app/apple-dock/page.tsx
+++ src/app/apple-dock/page.tsx
@@ -1,4 +1,14 @@
+"use client";
+
 import { TypographyH2 } from "@/components/Typography";
+import {
+  motion,
+  MotionValue,
+  useMotionValue,
+  useSpring,
+  useTransform,
+} from "motion/react";
+import { useRef } from "react";
 
 export default function Page() {
   return (
@@ -12,17 +22,40 @@ export default function Page() {
 }
 
 function Dock() {
+  const mouseX = useMotionValue(Infinity);
+
   return (
-    <div className="flex items-center p-2 rounded-2-xl w-fit gap-2 px-6 mx-auto bg-neutral-400 ">
+    <div
+      onMouseMove={(e) => {
+        mouseX.set(e.pageX);
+      }}
+      onMouseLeave={() => mouseX.set(Infinity)}
+      className="flex items-end w-fit h-16 mx-auto gap-4 px-4 pb-3 rounded-2xl bg-neutral-400 "
+    >
       {[...Array(6)].map((_, index) => (
-        <DockItem key={index} />
+        <DockItem key={index} mouseX={mouseX} />
       ))}
     </div>
   );
 }
 
-function DockItem() {
+function DockItem({ mouseX }: { mouseX: MotionValue<number> }) {
+  const ref = useRef<HTMLDivElement>(null);
+  const distance = useTransform(mouseX, (value) => {
+    const bounds = ref.current?.getBoundingClientRect() ?? { x: 0, width: 0 };
+    return value - bounds.x - bounds.width / 2;
+  });
+  const widthSync = useTransform(distance, [-200, 0, 200], [40, 80, 40]);
+  const width = useSpring(widthSync, {
+    mass: 0.2,
+    stiffness: 200,
+    damping: 15,
+  });
   return (
-    <div className="bg-neutral-500 flex items-center rounded-full h-12 w-12"></div>
+    <motion.div
+      ref={ref}
+      style={{ width }}
+      className="bg-neutral-500 flex items-center rounded-full w-10 aspect-square"
+    />
   );
 }
